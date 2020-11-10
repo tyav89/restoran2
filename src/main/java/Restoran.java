72961b85a98e5e23bab4ef1ffcb6930b7649c558
Dmitriy Tyavin <tyav89@mail.ru>
2020-11-10 16:39:22 +0300

    Change file Restoran.java

--- src/main/java/Restoran.java
+++ src/main/java/Restoran.java
@@ -7,50 +7,44 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Restoran {
     final int timeSleep = 2000;
     List<String> listCustomer = new ArrayList<>();
-    List<String> listOrder = new ArrayList<>();
     String name;
     private final Lock lock1 = new ReentrantLock();
     private final Lock lock2 = new ReentrantLock();
+    private final Lock lock3 = new ReentrantLock();
+
     Condition condition1 = lock1.newCondition();
     Condition condition2 = lock2.newCondition();
+    Condition condition3 = lock3.newCondition();
 
 
     public void make() {
         lock1.lock();
         try {
-            while (listCustomer.size() < 1) {
-                condition1.await();
-            }
+            condition1.await();
             name = listCustomer.remove(0);
             System.out.printf("Официант %s принял заказ у %s\n", Thread.currentThread().getName(), name);
-            waiterCook(name);
-            System.out.printf("Официант %s несет заказ %s\n", Thread.currentThread().getName(), name);
-            condition1.signal();
+            waiterCook();
+            carries();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             lock1.unlock();
 
         }
-
     }
 
     public void take() {
-        lock1.lock();
+        readyToOrder();
+        lock3.lock();
         try {
-            condition1.signal();
-            while (listOrder.size() < 1) {
-                condition1.await();
-            }
-            listOrder.remove(0);
+            condition3.await();
             System.out.printf("Посетитель %s ест\n", Thread.currentThread().getName());
             sleep();
             System.out.printf("Посетитель %s уходит\n", Thread.currentThread().getName());
-            condition1.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock1.unlock();
+            lock3.unlock();
         }
     }
 
@@ -77,18 +71,45 @@ public class Restoran {
         }
     }
 
-    public void waiterCook(String name) {
+    private void waiterCook() {
         lock2.lock();
         try {
             condition2.signal();
-            sleep();
-            listOrder.add(name);
+        } finally {
+            lock2.unlock();
+        }
+    }
+
+    private void orderReady() {
+        lock3.lock();
+        try {
+            condition3.signal();
+        } finally {
+            lock3.unlock();
+        }
+    }
+
+    private void readyToOrder() {
+        lock1.lock();
+        try {
+            condition1.signal();
+        } finally {
+            lock1.unlock();
+        }
+    }
+
+    private void carries() {
+        lock2.lock();
+        try {
             condition2.await();
+            System.out.printf("Официант %s несет заказ %s\n", Thread.currentThread().getName(), name);
+            orderReady();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             lock2.unlock();
         }
-
     }
+
+
 }
